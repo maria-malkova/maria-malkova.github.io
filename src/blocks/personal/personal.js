a12e1a69a9f4fb8de6b8550b12fe66d4c793c788
Maria Malkova <79992031684@yandex.ru>
2024-01-09 22:12:54 +0300

    Проверка

--- src/blocks/personal/personal.js
+++
@@ -1,8 +0,0 @@
-// Находим нужную кнопку
-const btns = document.querySelector('.personal__msymbol');
-
-// Прослушиваем нажатие на кнопку
-btns.addEventListener('click', function() {
-    // Переключаем (добавляем/удаляем) класс .dark-theme
-    document.body.classList.toggle('dark-theme'); 
-})
